41f96a4ccf8ae66e64a4c46e30771300b4f0534b
cry <1046707850@qq.com>
2024-02-15 15:48:32 +0800

    将ca从backend中抽取出

--- path_ca.go
+++ path_ca.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/x509"
 	"crypto/x509/pkix"
+	"encoding/json"
 	"fmt"
 	"github.com/hashicorp/vault/helper/namespace"
 	"math/big"
@@ -24,6 +25,7 @@ const (
 // required to instantiate a new HashiCups client.
 type CA struct {
 	L          *sync.RWMutex
+	ParentCaSn string            `json:"parent_ca_sn"`
 	CertPEM    string            `json:"cert_pem"`
 	CertBytes  []byte            `json:"cert_bytes"`
 	Cert       *x509.Certificate `json:"cert"`
@@ -70,22 +72,28 @@ func (b *KmipBackend) handleCAExistenceCheck() framework.ExistenceFunc {
 		if err != nil {
 			return false, fmt.Errorf("existence check failed: %w", err)
 		}
-		b.once.Do(func() { b.init(ctx, req) })
 		return out != nil, nil
 	}
 }
 
 func (b *KmipBackend) handleCARead() framework.OperationFunc {
 	return func(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-
-		b.rootCA[0].L.RLock()
-		rootCertPEM := b.rootCA[0].CertPEM
-		b.rootCA[0].L.RUnlock()
-		b.rootCA[1].L.RLock()
-		childCertPEM := b.rootCA[1].CertPEM
-		b.rootCA[1].L.RUnlock()
+		sn, err := listStorage(ctx, req, caPath)
+		if err != nil {
+			return nil, err
+		}
+		// read cert pem
+		pem := ""
+		ca := new(CA)
+		for _, k := range sn {
+			ca.readStorage(ctx, req, caPath, k)
+			if err != nil {
+				return nil, err
+			}
+			pem = pem + ca.CertPEM
+		}
 		resData := map[string]interface{}{
-			"ca_pem": rootCertPEM + childCertPEM,
+			"ca_pem": pem,
 		}
 		resp := &logical.Response{
 			Secret: &logical.Secret{},
@@ -93,36 +101,61 @@ func (b *KmipBackend) handleCARead() framework.OperationFunc {
 		}
 		return resp, nil
 	}
+
 }
 
-func (b *KmipBackend) addRootCAChain(index int, certPEM string, certBytes []byte, cert *x509.Certificate, privateKey interface{}) {
-	if index >= 0 {
-		b.rootCA[index].L.Lock()
-		b.rootCA[index].CertBytes = certBytes
-		b.rootCA[index].CertPEM = certPEM
-		b.rootCA[index].Cert = cert
-		b.rootCA[index].PrivateKey = privateKey
-		b.rootCA[index].L.Unlock()
-		return
+func (c *CA) setCA(certPEM string, certBytes []byte, cert *x509.Certificate, privateKey interface{}, sn *big.Int) {
+	c.Cert = cert
+	c.CertPEM = certPEM
+	c.PrivateKey = privateKey
+	c.CertBytes = certBytes
+	if sn == nil {
+		c.ParentCaSn = ""
+	} else {
+		c.ParentCaSn = sn.String()
 	}
-	CA := CA{
-		L:          new(sync.RWMutex),
-		CertPEM:    certPEM,
-		CertBytes:  certBytes,
-		Cert:       cert,
-		PrivateKey: privateKey,
-	}
-	b.rootCA = append(b.rootCA, &CA)
 }
 
-func (b *KmipBackend) SetCACert(role, scope, ttl string, ns *namespace.Namespace) (*big.Int, *x509.Certificate) {
+func (c *CA) readStorage(ctx context.Context, req *logical.Request, key string, sn string) error {
+	path := key
+	if !strings.HasSuffix(key, "/") {
+		path = path + "/"
+	}
+	path = path + sn
+	data, err := readStorage(ctx, req, path)
+	if err != nil {
+		return err
+	}
+	if err := MapToStruct(data, c); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *CA) writeStorage(ctx context.Context, req *logical.Request, key string) error {
+	buf, err := json.Marshal(c)
+	if err != nil {
+		return fmt.Errorf("json encoding failed: %w", err)
+	}
+
+	if !strings.HasSuffix(key, "/") {
+		key = key + "/"
+	}
+	// Write out a new key
+	entry := &logical.StorageEntry{
+		Key:   key + c.Cert.SerialNumber.String(),
+		Value: buf,
+	}
+	if err := req.Storage.Put(ctx, entry); err != nil {
+		return fmt.Errorf("failed to write: %w", err)
+	}
+	return nil
+}
+
+func SetCACert(role, scope, ttl string, ns *namespace.Namespace, sn *SerialNumber) *x509.Certificate {
 	duration, _ := time.ParseDuration(ttl)
-	b.SerialNumber.L.Lock()
-	SerialNumber := b.SerialNumber.SN
-	b.SerialNumber.SN.Add(b.SerialNumber.SN, big.NewInt(1))
-	b.SerialNumber.L.Unlock()
 	rootCert := &x509.Certificate{
-		SerialNumber: SerialNumber,
+		SerialNumber: sn.SN,
 		Subject: pkix.Name{
 			CommonName:   role,                     // role
 			Organization: []string{scope, ns.Path}, // scope
@@ -134,7 +167,60 @@ func (b *KmipBackend) SetCACert(role, scope, ttl string, ns *namespace.Namespace
 		BasicConstraintsValid: true,
 		IsCA:                  true,
 	}
-	return SerialNumber, rootCert
+	return rootCert
+}
+
+func (sn *SerialNumber) readStorage(ctx context.Context, req *logical.Request) error {
+	data, err := readStorage(ctx, req, serialNumberPath)
+	var snOld SerialNumber
+	if err != nil {
+		// SerialNumber not initialized
+		if err.Error() == errPathDataIsEmpty {
+			snOld = SerialNumber{
+				L:  new(sync.RWMutex),
+				SN: big.NewInt(0),
+			}
+		} else {
+			return err
+		}
+	} else {
+		MapToStruct(data, &snOld)
+	}
+	// 将sn
+	snNew := snOld
+	snNew.SN.Add(snNew.SN, big.NewInt(1))
+	buf, err := json.Marshal(snNew)
+	if err != nil {
+		return fmt.Errorf("json encoding failed: %w", err)
+	}
+
+	// Write out a new key
+	entry := &logical.StorageEntry{
+		Key:   serialNumberPath,
+		Value: buf,
+	}
+	if err := req.Storage.Put(ctx, entry); err != nil {
+		return fmt.Errorf("failed to write: %w", err)
+	}
+	sn.SN = snOld.SN
+	return nil
+}
+
+func (sn *SerialNumber) writeStorage(ctx context.Context, req *logical.Request) error {
+	buf, err := json.Marshal(sn)
+	if err != nil {
+		return fmt.Errorf("json encoding failed: %w", err)
+	}
+
+	// Write out a new key
+	entry := &logical.StorageEntry{
+		Key:   serialNumberPath,
+		Value: buf,
+	}
+	if err := req.Storage.Put(ctx, entry); err != nil {
+		return fmt.Errorf("failed to write: %w", err)
+	}
+	return nil
 }
 
 // pathConfigHelpSynopsis summarizes the help text for the configuration
